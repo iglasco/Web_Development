8f430e089f1be04c8869914d7a1fcda30ce173e1
iglasco <imani.glasco@gmail.com>
2022-03-12 10:45:28 -0600

    chalkboard project

---
+++ Chalkboard/script.js
@@ -0,0 +1,107 @@
+(function(){
+    console.log("purple");
+})();
+
+(function(){
+    console.log("black");
+})();
+
+(function(){
+    console.log("blood red");
+})();
+
+(function(){
+    console.log("royal blue")
+})();
+
+(function(){
+    console.log("pink")
+})(); 
+
+
+function animal () {
+    console.log("puppies");
+}
+animal();
+
+function animal() {
+    console.log("kittens");
+}
+animal();
+
+function animal() {
+    console.log("snakes");
+}
+animal();
+
+
+function animal() {
+    console.log("bunnies")
+}
+animal();
+
+function animal() {
+    console.log("ferrets")
+}
+animal();
+
+function cities() {
+    console.log("Chicago");
+}
+cities();
+
+function cities() {
+    console.log("Houston");
+}
+cities();
+
+function cities() {
+    console.log("Rome");
+}
+cities();
+
+function cities() {
+    console.log("Paris");
+}
+cities();
+
+function cities() {
+    console.log("Monaco")
+}
+cities();
+
+function favBook(name) {
+    console.log("I hear your favorite book is " + name + ".");
+}
+
+favBook("Mistborn");
+
+function favBook(name) {
+    console.log("I hear your favorite book is " + name + ".");
+} 
+
+favBook("The Well of Ascension");
+
+function favBook(name) {
+    console.log("I hear your favorite book is " + name + ".");
+}
+
+favBook("All Systems Red");
+
+function favBook(name) {
+    console.log("I hear your favorite book is " + name + ".");
+}
+
+favBook("The Name of the Wind");
+
+function favBook(name) {
+    console.log("I hear your favorite book is " + name + ".");
+}
+
+favBook("The Seven Husbands of Evelyn Hugo");
+
+function money(amount, name) {
+    console.log("Hey " + name + " can I borrow $" + amount + " from you?");
+}
+
+money(1000 , "Ryan");
